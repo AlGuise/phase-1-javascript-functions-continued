65ee4dd6d2c6e5672a1712334a323614130bed5a
Alain Giessel <alain.giessel@gmail.com>
2022-02-23 19:46:09 -0700

    commit tree bd991a69bb78a82c0cef02c576a6ce51384cacf3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(workDayActivity = 'go to the office') {
+    return `This Monday, I will ${workDayActivity}.`
+}
+
+function wrapAdjective(symbol) {
+    const part1 = `You are `;
+    return function (wordWrap) {
+        const part2 = `${symbol}${wordWrap}${symbol}!`
+        return (part1 + part2)
+    }
+}
